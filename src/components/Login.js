94c3e769d0b46aed9a5acb0473d57f146a8cec04
gregdrew12 <gregdrew12@gmail.com>
2023-10-18 00:14:36 -0400

    Lower bootstrap priority and add basic styling

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,6 +2,7 @@
 import axios from "axios";
 import {useState} from "react";
 import { API_URL } from "../constants";
+import './Login.css';
 
 // Define the Login function.
 export const Login = () => {
