06549e30f4843c11a72e71ecba9f1f0bc1f65e62
gregdrew12 <gregdrew12@gmail.com>
2023-10-28 21:59:16 -0400

    Add loading screen to profile pages

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,34 +1,36 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from 'react-router-dom';
-import { Col, Container, Row } from "reactstrap";
-import UserList from "./UserList";
-import NewUserModal from "./NewUserModal";
 
 import axios from "axios"
 
 import { API_URL } from "../constants";
+import Loading from "./Loading";
 
 const Profile = () => {
+    const [userLoading, setUserLoading] = useState(true);
     const [user, setUser] = useState([]); 
     const { username } = useParams();
 
     useEffect(() => {
-        console.log(username)
         axios.get(API_URL + 'api/users/', {
             params: {
                 username: username
             }
-        }).then(res => setUser(res.data));
+        }).then(res => {
+            setUser(res.data);
+            setUserLoading(false);
+        });
     }, []);
 
     return (
         <>
-            {user.length === 1 ? (
-                <div>
-                    <h1>{user[0].username}</h1>
-                    <h2></h2>
-                </div>
-            ) : null}
+            {userLoading ? <Loading/> :
+                user.length === 1 ? (
+                    <div>
+                        <h1>{user[0].username}</h1>
+                        <h2></h2>
+                    </div>
+            ) : <h1>User does not exist.</h1>}
         </>
     )
 }
